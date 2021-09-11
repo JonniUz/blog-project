663097a46022232de2acc1bdd5e3611d97bdb8d8
jonniuz <javoxirbek.uzb@gmail.com>
2021-09-11 12:03:06 -0400

    First Commit

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.PostListView.as_view(), name='blog-home'),
-    path('about/', views.about, name='blog-about'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    ]
