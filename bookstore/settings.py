b24d49f4fb6a3539b25f759cade9c401bb872959
jonniuz <javoxirbek.uzb@gmail.com>
2021-09-15 20:54:26 -0400

    allow host added .

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookstore.wsgi.application'
 
-import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 if os.environ.get('DATABASE_URL'):
