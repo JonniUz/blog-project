4fb9eecb69df45b0346f5b9e115e250eecba88ca
jonniuz <javoxirbek.uzb@gmail.com>
2021-09-16 02:33:14 -0400

    allow host added .

--- books/models.py
+++ books/models.py
@@ -18,6 +18,7 @@ class Book(models.Model):
     shortDescription = models.CharField(max_length=2505, null=True)
     longDescription = models.TextField(null=True)
     authors = models.ManyToManyField('Author')
+    thumbnail = models.CharField(max_length=2055)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
     added_at = models.DateTimeField(auto_now_add=True)
 
